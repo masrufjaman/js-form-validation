d4e99c1214c3628b5ab4069dee2e1108b38877a9
Md. Masruf Jaman <masruf.jaman@northsouth.edu>
2021-08-03 16:43:40 +0600

    pop up modal

--- script.js
+++ script.js
@@ -102,3 +102,12 @@ function otherValidation() {
     }   
      
 }
+
+// Modal Section
+document.getElementById('button').addEventListener('click', function() {
+    document.querySelector('.bg-modal').style.display = 'flex';    
+})
+
+document.querySelector('.close').addEventListener('click', function() {
+    document.querySelector('.bg-modal').style.display = 'none';
+})
